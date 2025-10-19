1be23fba6bf0903ea523b286c5df17c6647b1af1
Vvk_0105 <vivekbabu.sics@gmail.com>
2025-10-19 15:37:58 +0530

    change the baseurl

--- src/config.js
+++ src/config.js
@@ -1,3 +1,4 @@
-const BASE_URL = "http://localhost:8000/api";
+const BASE_URL =
+  window.location.hostname === "localhost" ? "http://localhost:8000/api" : "https://paradise-hotel-api.onrender.com/api";
 
-export default BASE_URL
+export default BASE_URL;
